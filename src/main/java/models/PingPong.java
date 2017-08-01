ecd3df008ca8fc8e17dda850bc6925eaa0a57438
Carson Canales and Kira Sarnoff-Loo <kira@>
2017-08-01 10:33:41 -0700

    second and third failed tests, added ping

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -8,7 +8,13 @@ public class PingPong {
 
     public List<Object> runPingPong(int countUpTo){
         List<Object> result = new ArrayList<Object>();
-        return null;
-
+        for (int i = 1; i <= countUpTo; i++) {
+            if (i % 3 == 0) {
+                result.add("ping");
+            } else {
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
