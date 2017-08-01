f9be411fb9ceeb3ecceb262081ea7bbf5213e596
Carson Canales and Kira Sarnoff-Loo <carson@>
2017-08-01 10:49:36 -0700

    added ping and pingpong, ran test 4 & 5

--- src/test/java/models/PingPongTest.java
+++ src/test/java/models/PingPongTest.java
@@ -15,9 +15,7 @@ public class PingPongTest {
     public void runPingPong_countUpToOne_ArrayList() {
         PingPong testPingPong = new PingPong();
         ArrayList<Object> expectedOutput = new ArrayList<Object>();
-        expectedOutput.add(1);
-        assertEquals(expectedOutput, testPingPong.runPingPong(1));
-
+        expectedOutput.add(1);assertEquals(expectedOutput, testPingPong.runPingPong(1));
     }
 
     @Test
@@ -38,4 +36,37 @@ public class PingPongTest {
         expectedOutput.add("ping");
         assertEquals(expectedOutput, testPingPong.runPingPong(3));
     }
+    @Test
+    public void runPingPong_replaceMultiplesOf5_ArrayList() {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        assertEquals(expectedOutput, testPingPong.runPingPong(5));
+    }
+    @Test
+    public void runPingPong_replaceMultiplesOfBoth3And5_ArrayList() {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        expectedOutput.add("ping");
+        expectedOutput.add(7);
+        expectedOutput.add(8);
+        expectedOutput.add("ping");
+        expectedOutput.add("pong");
+        expectedOutput.add(11);
+        expectedOutput.add("ping");
+        expectedOutput.add(13);
+        expectedOutput.add(14);
+        expectedOutput.add("pingpong");
+        assertEquals(expectedOutput, testPingPong.runPingPong(15));
+    }
+
 }
